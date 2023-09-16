4b768c0c14e751adc7d35ea2999860d64aef3ed0
lord-balaskandar <thanushenbalaskandar@gmail.com>
2023-09-16 16:25:19 +0000

    test

--- src/tests/ui/app/app.test.tsx
+++ src/tests/ui/app/app.test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../../../ui/app/app';
 
+/**
+ * Test that react flow appears
+ */
 test('reactFlow', () => {
   render(<App />);
   expect(screen.getByTestId('rf__wrapper')).toHaveClass('react-flow');
